ca5faf1944ab84c30c513292b2977a0294104658
kotahorii <khorii.micron@gmail.com>
2022-03-04 22:06:29 +0900

    update file

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 class Main {
   public static void main(String[] args) {
-    String name = "takashi";
-    int age = 25;
-    double height = 170;
-    double weight = 70;
-    double bmi = weight / height / height;
-    System.out.println("名前は" + name + "です");
-    System.out.println("年齢は" + age + "歳です");
-    System.out.println("身長は" + height + "mです");
-    System.out.println("体重は" + weight + "kgです");
-    System.out.println("BMIは" + bmi + "です");
+    System.out.println("=== while文 ===");
+    int i = 1;
+    while (i < 10) {
+      if (i == 5) {
+        break;
+      }
+      System.out.println(i);
+      i++;
+    }
 
+    System.out.println("=== for文 ===");
+    for (int j = 1; j < 10; j++) {
+      if (j % 3 == 0) {
+        break;
+      }
+      System.out.println(j);
+    }
   }
 }
