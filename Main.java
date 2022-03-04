48c7cff9030bc10eee7dd16e805f3fb335451482
kotahorii <khorii.micron@gmail.com>
2022-03-04 21:33:19 +0900

    update file

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 class Main {
   public static void main(String[] args) {
-    int length = 6;
-    int height = 8;
+    int number1 = 7;
+    int number2 = 2;
+    System.out.println(number1 / number2);
+    System.out.println((double) number1 / number2);
 
-    int rectangleArea = length * height;
-    int triangleArea = rectangleArea / 2;
-    System.out.println(rectangleArea);
-    System.out.println(triangleArea);
   }
 }
