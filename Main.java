717697604f6904f21d09624b45f5653cf59f584b
kotahorii <khorii.micron@gmail.com>
2022-03-04 21:44:25 +0900

    update file

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    int number1 = 7;
-    int number2 = 2;
-    System.out.println(number1 / number2);
-    System.out.println((double) number1 / number2);
+    String name = "takashi";
+    int age = 25;
+    double height = 170;
+    double weight = 70;
+    double bmi = weight / height / height;
+    System.out.println("名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    System.out.println("BMIは" + bmi + "です");
 
   }
 }
